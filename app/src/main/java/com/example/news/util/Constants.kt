65db9e63d7e1ef19ad1f705d5a4f2852e9358a71
Doğan Ensar Papuçcuoğlu <93671880+archonie@users.noreply.github.com>
2024-08-20 16:10:54 +0300

    Update Constants.kt

--- app/src/main/java/com/example/news/util/Constants.kt
+++ app/src/main/java/com/example/news/util/Constants.kt
@@ -6,8 +6,6 @@ object Constants {
 
     const val APP_ENTRY = "appEntry"
 
-    const val API_KEY = "67bdc4f3c0a449e7a6c5735204d9139d"
-
     const val BASE_URL = "https://newsapi.org/v2/"
 
     const val NEWS_DB = "news_db"
